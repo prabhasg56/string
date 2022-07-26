3b961c4e3078ce822ea6a436ae85baf5508c037b
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-26 11:26:09 +0000

    Calling function for String4

---
+++ Test/testString4.js
@@ -0,0 +1,4 @@
+const fullNameTitle = require("../string4");
+
+console.log(fullNameTitle({"first_name": "JoHN", "last_name": "SMith"}));
+console.log(fullNameTitle({"first_name": "JoHN", "middle_name": "doe", "last_name": "SMith"}));
